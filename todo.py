2db07928e9fb95c33c66435f87d8339656f38a02
theylovecjawn <cjohn@brooklynsteamcenter.com>
2024-01-02 10:53:50 -0500

    Benchmarck 2

--- todo.py
+++ todo.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 todos = ["Make the basketball team", "Get 18 Certifications"]
 
-@app.route('/', methods=['GET','POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    new_todo = request.form['new_todo']
-    return render_template('todo.html.jinja', todos = todos)
+    if request.method == "POST":
+        new_todo = request.form["new_todo"].strip() 
+        if new_todo:  
+            todos.append(new_todo)
+    return render_template("todo.html.jinja", todos=todos)
 
+@app.route('/delete_todo/<int:todo_index>', methods=['POST'])
+def todo_delete(todo_index):
+    del todos[todo_index]
+    return redirect('/')
 
-
-
+if __name__ == "__main__":
+    app.run(debug=True)
